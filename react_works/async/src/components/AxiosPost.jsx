1c015de516b0d29ced194e3b649747619ad50cb8
sudo2100 <sudo2100@naver.com>
2026-06-27 14:01:36 +0900

    axios post

--- react_works/async/src/components/AxiosPost.jsx
+++ react_works/async/src/components/AxiosPost.jsx
@@ -10,17 +10,42 @@ const AxiosPost = () => {
     setTitle(e.target.value);
   }
 
-  return(
+  //폼 전송시 핸들러(함수)
+  const handleSubmit = (e) => {
+    e.preventDefault();  //새로고침 방지
+
+    //유효성 검사
+    if (!title) { //제목에 텍스트가 없으면
+      alert("할 일을 입력하세요");
+      return;
+    }
+
+    //할 일 생성
+    axios.post("https://jsonplaceholder.typicode.com/todos", {
+      title: title,  //제목
+      completed: false  //완료상태
+    })
+      .then((res) => {
+        console.log("추가된 데이터:", res.data);
+        alert("추가 완료!!");
+        setTitle("");  //입력 필드 초기화
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
     <div>
       <h2>할 일 추가</h2>
-      <input 
-        type="text" 
-        placeholder="할 일 입력"
-        value={title}
-        onChange={handleInputChange}
-      />
-      <button>추가</button>
-      <p>입력된 할일: {title}</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="할 일 입력"
+          value={title}
+          onChange={handleInputChange}
+        />
+        <button type="submit">추가</button>
+      </form>
+      {/* <p>입력된 할일: {title}</p> */}
     </div>
   )
 }
